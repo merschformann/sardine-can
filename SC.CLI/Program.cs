4858b2b5e7358c803b1062c7a2513f5993028846
Marius Merschformann <marius.merschformann@gmail.com>
2021-09-12 01:09:12 +0200

    Adding cli project, updating packages

---
+++ SC.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SC.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
